8ae6dea4a227e6f6bd39aa5e04a61be262db4b22
balviguy267 <balviguy267@gmail.com>
2025-03-12 08:01:53 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define an array with some animals
+$animals = array("Cat", "Dog", "Lion", "Elephant");
+
+// Loop through the array and display each animal
+foreach ($animals as $animal) {
+    echo "$animal<br>";
+}
+
+?>
